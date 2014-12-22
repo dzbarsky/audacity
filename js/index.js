2f94573f880a724cc83bb349fb1629cc9c22fa11
David Zbarsky <dzbarsky@gmail.com>
2014-12-21 20:50:17 -0500

    Refactor to allow playing back multiple tracks

--- js/index.js
+++ js/index.js
@@ -2,10 +2,88 @@ window.addEventListener('load', setup);
 
 var state;
 var context;
-var currentTrackDiv;
-var currentDrawn = 0;
 var SAMPLES_PER_PIXEL = 200;
-var finalBuffer;
+
+var currentTrack;
+var tracks = []
+
+function AudioTrack() {
+  var domElement = document.createElement("div");
+  document.getElementById('tracks').appendChild(domElement);
+
+  this.canvas = document.createElement("canvas");
+  domElement.appendChild(this.canvas);
+
+  this.canvas.width = 0;
+  this.canvas.height = 100;
+  this.canvas.style.border = '1px solid red';
+
+  this.buffers = [];
+  this.currentDrawn = 0;
+  this.finalBuffer = null;
+}
+
+AudioTrack.prototype.consolidateAudioBuffers = function() {
+  var totalLength = 0;
+  this.buffers.forEach(function (buffer) {
+    totalLength += buffer.length;
+  });
+
+  this.finalBuffer = context.createBuffer(2, totalLength, this.buffers[0].sampleRate);
+  for (var i = 0; i < 2; i++) {
+    var channel = this.finalBuffer.getChannelData(i);
+
+    var currentLength = 0;
+    this.buffers.forEach(function (buffer) {
+      channel.set(buffer.getChannelData(i), currentLength);
+      currentLength += buffer.length;
+    });
+  }
+};
+
+AudioTrack.prototype.addBufferSegment = function(bufferSegment) {
+  this.buffers.push(bufferSegment);
+
+  var canvasContext = this.canvas.getContext('2d');
+  var oldWidth = this.canvas.width;
+  if (oldWidth !== 0) {
+    var data = canvasContext.getImageData(0, 0, this.canvas.width, this.canvas.height);
+  }
+  this.canvas.width += bufferSegment.length / SAMPLES_PER_PIXEL;
+
+  if (oldWidth !== 0) {
+    canvasContext.putImageData(data, 0, 0);
+  }
+
+  this.drawBuffer(canvasContext,
+                  bufferSegment.getChannelData(0));
+};
+
+AudioTrack.prototype.drawBuffer = function(context, data) {
+  var amp = this.canvas.height / 2;
+  context.fillStyle = "silver";
+  for (var i = 0; i < this.canvas.width - this.currentDrawn; i++){
+    var min = 1.0;
+    var max = -1.0;
+    for (j = 0; j < SAMPLES_PER_PIXEL; j++) {
+      var datum = data[(i * SAMPLES_PER_PIXEL) + j];
+      if (datum < min)
+        min = datum;
+      if (datum > max)
+        max = datum;
+     }
+     context.fillRect(i + this.currentDrawn, (1 + min) * amp,
+                      1, Math.max(1, (max - min) * amp));
+  }
+  this.currentDrawn += data.length / SAMPLES_PER_PIXEL;
+};
+
+AudioTrack.prototype.play = function() {
+  var source = context.createBufferSource();
+  source.buffer = this.finalBuffer;
+  source.connect(context.destination);
+  source.start();
+};
 
 function setup() {
   document.getElementById('record').addEventListener('click', toggleRecord);
@@ -25,54 +103,20 @@ function toggleRecord() {
   if (state === 'recording') {
     document.getElementById('record').src = 'images/record.png';
     state = 'stopped';
-    createFinalBuffer();
+    currentTrack.consolidateAudioBuffers();
   } else {
     document.getElementById('record').src = 'images/stop.png';
     state = 'recording';
-
-    currentTrackDiv = document.createElement("div");
-    currentDrawn = 0;
-    document.getElementById('tracks').appendChild(currentTrackDiv);
-
-    var canvas = document.createElement("canvas");
-    currentTrackDiv.appendChild(canvas);
-
-    canvas.width = 0;
-    canvas.height = 100;
-    canvas.style.border = '1px solid red';
-
-
-    buffers = [];
-  }
-}
-
-function createFinalBuffer() {
-
-  var totalLength = 0;
-  buffers.forEach(function (buffer) {
-    totalLength += buffer.length;
-  });
-
-  finalBuffer = context.createBuffer(2, totalLength, buffers[0].sampleRate);
-  for (var i = 0; i < 2; i++) {
-    var channel = finalBuffer.getChannelData(i);
-
-    var currentLength = 0;
-    buffers.forEach(function (buffer) {
-      channel.set(buffer.getChannelData(i), currentLength);
-      currentLength += buffer.length;
-    });
+    currentTrack = new AudioTrack();
+    tracks.push(currentTrack);
   }
 }
 
 function play() {
-  var source = context.createBufferSource();
-  source.buffer = finalBuffer;
-  source.connect(context.destination);
-  source.start();
+  tracks.forEach(function(track) {
+    track.play();
+  });
 }
-
-var buffers = [];
 
 function success(audioStream) {
   context = new AudioContext();
@@ -82,49 +126,13 @@ function success(audioStream) {
 
   var scriptProcessor = context.createScriptProcessor(4096, 2, 2);
   scriptProcessor.onaudioprocess = function(e) {
-    if (state !== 'recording') {
-      return;
+    if (state === 'recording') {
+      currentTrack.addBufferSegment(e.inputBuffer);
     }
-    console.log('recording');
-    buffers.push(e.inputBuffer);
-
-    var canvas = currentTrackDiv.firstChild;
-
-    var canvasContext = canvas.getContext('2d');
-    var oldWidth = canvas.width;
-    if (oldWidth !== 0) {
-      var data = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
-    }
-    canvas.width += e.inputBuffer.length / SAMPLES_PER_PIXEL;
-
-    if (oldWidth !== 0) {
-      canvasContext.putImageData(data, 0, 0);
-    }
-    drawBuffer(canvas.width,
-               canvas.height,
-               canvasContext,
-               e.inputBuffer.getChannelData(0));
   };
 
   streamSource.connect(scriptProcessor);
   scriptProcessor.connect(context.destination);
 }
 
-function drawBuffer(width, height, context, data) {
-  var amp = height / 2;
-  context.fillStyle = "silver";
-  for (var i = 0; i < width - currentDrawn; i++){
-    var min = 1.0;
-    var max = -1.0;
-    for (j = 0; j < SAMPLES_PER_PIXEL; j++) {
-      var datum = data[(i * SAMPLES_PER_PIXEL) + j];
-      if (datum < min)
-        min = datum;
-      if (datum > max)
-        max = datum;
-     }
-     context.fillRect(i + currentDrawn, (1 + min) * amp,
-                      1, Math.max(1, (max - min) * amp));
-  }
-  currentDrawn += data.length / SAMPLES_PER_PIXEL;
-}
+
